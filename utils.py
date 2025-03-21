2344ac4643846877af5082e9a5653b7ad72d2078
dylanglenister <117338067+DylanGlenister@users.noreply.github.com>
2025-03-22 00:40:50 +1100

    Converted indentation to tabs

--- utils.py
+++ utils.py
@@ -64,8 +64,8 @@ import numpy as np
 #    return next(iter(iterable), default)
 
 def is_in(elt, seq):
-    """Similar to (elt in seq), but compares with 'is', not '=='."""
-    return any(x is elt for x in seq)
+	"""Similar to (elt in seq), but compares with 'is', not '=='."""
+	return any(x is elt for x in seq)
 
 #def mode(data):
 #    """Return the most common data item. If there are ties, return any one of them."""
@@ -311,10 +311,10 @@ def is_in(elt, seq):
 #    return turn_heading(heading, LEFT)
 
 def distance(a, b):
-    """The distance between two (x, y) points."""
-    xA, yA = a
-    xB, yB = b
-    return np.hypot((xA - xB), (yA - yB))
+	"""The distance between two (x, y) points."""
+	xA, yA = a
+	xB, yB = b
+	return np.hypot((xA - xB), (yA - yB))
 
 #def distance_squared(a, b):
 #    """The square of the distance between two (x, y) points."""
@@ -341,27 +341,27 @@ def distance(a, b):
 
 # Edited
 def memoize(fn, slot=None, maxsize=32):
-    """Memoize fn: make it remember the computed value for any argument list.
-    If slot is specified, store result in that slot of first argument.
-    If slot is false, use lru_cache for caching the values."""
-    if slot:
-        def memoized_normal(obj, *args):
-            if hasattr(obj, slot):
-                return getattr(obj, slot)
-            else:
-                val = fn(obj, *args)
-                setattr(obj, slot, val)
-                return val
+	"""Memoize fn: make it remember the computed value for any argument list.
+	If slot is specified, store result in that slot of first argument.
+	If slot is false, use lru_cache for caching the values."""
+	if slot:
+		def memoized_normal(obj, *args):
+			if hasattr(obj, slot):
+				return getattr(obj, slot)
+			else:
+				val = fn(obj, *args)
+				setattr(obj, slot, val)
+				return val
 
-        out = memoized_normal
-    else:
-        @functools.lru_cache(maxsize=maxsize)
-        def memoized_cached(*args):
-            return fn(*args)
+		out = memoized_normal
+	else:
+		@functools.lru_cache(maxsize=maxsize)
+		def memoized_cached(*args):
+			return fn(*args)
 
-        out = memoized_cached
+		out = memoized_cached
 
-    return out
+	return out
 
 #def name(obj):
 #    """Try to find some reasonable name for the object."""
@@ -637,61 +637,61 @@ def memoize(fn, slot=None, maxsize=32):
 # PriorityQueue is implemented here
 
 class PriorityQueue:
-    """A Queue in which the minimum (or maximum) element (as determined by f and
-    order) is returned first.
-    If order is 'min', the item with minimum f(x) is
-    returned first; if order is 'max', then it is the item with maximum f(x).
-    Also supports dict-like lookup."""
+	"""A Queue in which the minimum (or maximum) element (as determined by f and
+	order) is returned first.
+	If order is 'min', the item with minimum f(x) is
+	returned first; if order is 'max', then it is the item with maximum f(x).
+	Also supports dict-like lookup."""
 
-    def __init__(self, order='min', f=lambda x: x):
-        self.heap = []
-        if order == 'min':
-            self.f = f
-        elif order == 'max':  # now item with max f(x)
-            self.f = lambda x: -f(x)  # will be popped first
-        else:
-            raise ValueError("Order must be either 'min' or 'max'.")
+	def __init__(self, order='min', f=lambda x: x):
+		self.heap = []
+		if order == 'min':
+			self.f = f
+		elif order == 'max':  # now item with max f(x)
+			self.f = lambda x: -f(x)  # will be popped first
+		else:
+			raise ValueError("Order must be either 'min' or 'max'.")
 
-    def append(self, item):
-        """Insert item at its correct position."""
-        heapq.heappush(self.heap, (self.f(item), item))
+	def append(self, item):
+		"""Insert item at its correct position."""
+		heapq.heappush(self.heap, (self.f(item), item))
 
-    def extend(self, items):
-        """Insert each item in items at its correct position."""
-        for item in items:
-            self.append(item)
+	def extend(self, items):
+		"""Insert each item in items at its correct position."""
+		for item in items:
+			self.append(item)
 
-    def pop(self):
-        """Pop and return the item (with min or max f(x) value)
-        depending on the order."""
-        if self.heap:
-            return heapq.heappop(self.heap)[1]
-        else:
-            raise Exception('Trying to pop from empty PriorityQueue.')
+	def pop(self):
+		"""Pop and return the item (with min or max f(x) value)
+		depending on the order."""
+		if self.heap:
+			return heapq.heappop(self.heap)[1]
+		else:
+			raise Exception('Trying to pop from empty PriorityQueue.')
 
-    def __len__(self):
-        """Return current capacity of PriorityQueue."""
-        return len(self.heap)
+	def __len__(self):
+		"""Return current capacity of PriorityQueue."""
+		return len(self.heap)
 
-    def __contains__(self, key):
-        """Return True if the key is in PriorityQueue."""
-        return any([item == key for _, item in self.heap])
+	def __contains__(self, key):
+		"""Return True if the key is in PriorityQueue."""
+		return any([item == key for _, item in self.heap])
 
-    def __getitem__(self, key):
-        """Returns the first value associated with key in PriorityQueue.
-        Raises KeyError if key is not present."""
-        for value, item in self.heap:
-            if item == key:
-                return value
-        raise KeyError(str(key) + " is not in the priority queue")
+	def __getitem__(self, key):
+		"""Returns the first value associated with key in PriorityQueue.
+		Raises KeyError if key is not present."""
+		for value, item in self.heap:
+			if item == key:
+				return value
+		raise KeyError(str(key) + " is not in the priority queue")
 
-    def __delitem__(self, key):
-        """Delete the first occurrence of key."""
-        try:
-            del self.heap[[item == key for _, item in self.heap].index(True)]
-        except ValueError:
-            raise KeyError(str(key) + " is not in the priority queue")
-        heapq.heapify(self.heap)
+	def __delitem__(self, key):
+		"""Delete the first occurrence of key."""
+		try:
+			del self.heap[[item == key for _, item in self.heap].index(True)]
+		except ValueError:
+			raise KeyError(str(key) + " is not in the priority queue")
+		heapq.heapify(self.heap)
 
 # ______________________________________________________________________________
 # Useful Shorthands
